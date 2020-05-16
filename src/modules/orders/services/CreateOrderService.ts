84d2d525fc1f5ee83fac0455df2af011bd4328eb
leandrosouzaa <leandror.dev@gmail.com>
2020-05-16 12:50:55 -0300

    create order feature

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,53 @@ interface IRequest {
 @injectable()
 class CreateProductService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const checkCustomerExist = await this.customersRepository.findById(
+      customer_id,
+    );
+
+    if (!checkCustomerExist) {
+      throw new AppError('Customer not exist');
+    }
+
+    const productsPrice = await this.productsRepository.findAllById(
+      products.map(p => ({ id: p.id })),
+    );
+
+    if (products.length !== productsPrice.length) {
+      throw new AppError('Product not exist');
+    }
+
+    products.forEach(p => {
+      const productQuantityInDatabase = productsPrice.find(
+        product => p.id === product.id,
+      )?.quantity;
+
+      if ((productQuantityInDatabase || 0) < p.quantity) {
+        throw new AppError('Send a valid quantity');
+      }
+    });
+
+    const order = await this.ordersRepository.create({
+      customer: checkCustomerExist,
+      products: products.map(product => ({
+        product_id: product.id,
+        price: productsPrice.find(({ id }) => id === product.id)?.price || 0,
+        quantity: product.quantity,
+      })),
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
